90dcc720095fba96b89cdf91b89f8775a6201fdd
yousuf7902 <yousufhassan04@gmail.com>
2024-11-28 22:56:11 +0600

    backend server create

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/Antopolis_Intern");
+
+app.listen(8080, () => {
+    console.log("Server is running on PORT 8080....");
+});
+

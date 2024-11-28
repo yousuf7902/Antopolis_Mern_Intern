605c596b70417f476c4e312193a9dad68f8fe6d1
yousuf7902 <yousufhassan04@gmail.com>
2024-11-28 23:18:21 +0600

    front-end header part added

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -6,6 +6,7 @@ const geistSans = localFont({
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
       >
         {children}
       </body>
